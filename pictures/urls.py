b3a48d8eaf008384b5c30fa14ed48b7fd7688948
Aquinology <riabova_e@iuca.kg>
2020-10-30 00:54:07 +0600

    PictureDetails

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import PictureList
+from .views import PictureList, PictureDetail
 
 
 urlpatterns = [
     path("", PictureList.as_view(), name="picture_list"),
+    path("<int:pk>/", PictureDetail.as_view(), name="picture_detail"),
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Picture
+from django.utils import timezone
 
 
 class PictureList(ListView):
+    model = Picture
+    context_object_name = "picture_list"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["date"] = timezone.now()
+    #     return context
+
+
+class PictureDetail(DetailView):
     model = Picture
