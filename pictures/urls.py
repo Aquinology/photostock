18e71e48f07fbe9c34b87a7ab3f45f2a2fce4b9c
Aquinology <riabova_e@iuca.kg>
2020-10-30 02:25:59 +0600

    Create, Delete, Update, LRM

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
-from .views import PictureList, PictureDetail
+from .views import PictureList, PictureDetail, PictureCreate, PictureDelete, PictureUpdate
 
 
 urlpatterns = [
     path("", PictureList.as_view(), name="picture_list"),
     path("<int:pk>/", PictureDetail.as_view(), name="picture_detail"),
+    path("delete/<int:pk>/", PictureDelete.as_view(), name="picture_delete"),
+    path("update/<int:pk>/", PictureUpdate.as_view(), name="picture_update"),
+    path("new/", PictureCreate.as_view(), name="picture_new"),
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Picture
 from django.utils import timezone
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class PictureList(ListView):
@@ -16,3 +19,20 @@ class PictureList(ListView):
 
 class PictureDetail(DetailView):
     model = Picture
+
+
+class PictureCreate(LoginRequiredMixin, CreateView):
+    model = Picture
+    fields = ["title", "picture", "desc"]
+    success_url = reverse_lazy("picture_list")
+
+
+class PictureDelete(LoginRequiredMixin, DeleteView):
+    model = Picture
+    success_url = reverse_lazy("picture_list")
+
+
+class PictureUpdate(LoginRequiredMixin, UpdateView):
+    model = Picture
+    fields = ["title", "picture", "desc"]
+    success_url = reverse_lazy("picture_list")
