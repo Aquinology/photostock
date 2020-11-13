082bea4c79c6a5593736de9e530642cb20090242
Aquinology <riabova_e@iuca.kg>
2020-11-13 17:11:10 +0600

    Download picture

--- pictures/views.py
+++ pictures/views.py
@@ -42,11 +42,18 @@ class PictureOwnerList(LoginRequiredMixin, ListView):
 class PictureDetail(DetailView):
     model = Picture
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            if Picture.objects.filter(user=self.request.user).exists():
+                context["download"] = True
+        return context
+
 
 class PictureCreate(LoginRequiredMixin, CreateView):
     model = Picture
     fields = ["type", "title", "picture"]
-    success_url = reverse_lazy("picture_list")
+    success_url = reverse_lazy("my_picture_list")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -55,20 +62,20 @@ class PictureCreate(LoginRequiredMixin, CreateView):
 
 class PictureDelete(LoginRequiredMixin, DeleteView):
     model = Picture
-    success_url = reverse_lazy("picture_list")
+    success_url = reverse_lazy("my_picture_list")
 
     def dispatch(self, request, *args, **kwargs):
         if self.request.user != self.get_object().user:
             raise Http404("Вы не можете удалять чужие добавления")
-        return super(PictureDelete, self).dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
 
 class PictureUpdate(LoginRequiredMixin, UpdateView):
     model = Picture
     fields = ["title", "picture"]
-    success_url = reverse_lazy("picture_list")
+    success_url = reverse_lazy("my_picture_list")
 
     def dispatch(self, request, *args, **kwargs):
         if self.request.user != self.get_object().user:
             raise Http404("Вы не можете редактировать чужие добавления")
-        return super(PictureUpdate, self).dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
